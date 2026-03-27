90d6ff718974c40fcbaff967f9f956d12377a0ae
maddawik <edwardreillycomputers@gmail.com>
2026-03-27 10:31:11 -0400

    chore: update output var
    Less brittle implementation that uses concat and one functions

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -23,7 +23,9 @@ output "for_directive" {
   value = "%{for i, name in var.user_names}(${i}): ${name}, %{endfor}"
 }
 
-# NOTE: this is a bit brittle...
 output "neo_cloudwatch_policy_arn" {
-  value = var.give_neo_full_cloudwatch_access ? aws_iam_user_policy_attachment.neo_cloudwatch_full_access[0].policy_arn : aws_iam_user_policy_attachment.neo_cloudwatch_read_only[0].policy_arn
+  value = one(concat(
+    aws_iam_user_policy_attachment.neo_cloudwatch_full_access[*].policy_arn,
+    aws_iam_user_policy_attachment.neo_cloudwatch_read_only[*].policy_arn,
+  ))
 }
